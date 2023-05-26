68d7022c2ea7566f78e09bf87b689193d1d3fc5e
Ramadito Ferdian Assa <ramaditoferdian@gmail.com>
2023-05-27 00:21:49 +0700

    [Listing Creation]
    - STEP 1 (Category Selection)
    - STEP 2 (Location selection, Map component, Country autocomplete)

--- app/components/elements/Navbar/UserMenu.tsx
+++ app/components/elements/Navbar/UserMenu.tsx
@@ -9,6 +9,7 @@ import useRegisterModal from '@/app/hooks/useRegisterModal';
 import useLoginModal from '@/app/hooks/useLoginModal';
 import { signOut } from 'next-auth/react';
 import { SafeUser } from '@/app/types';
+import useRentModal from '@/app/hooks/useRentModal';
 
 interface UserMenuProps {
   currentUser?: SafeUser | null;
@@ -19,17 +20,28 @@ const UserMenu: React.FC<UserMenuProps> = ({
 }) => {
   const registerModal = useRegisterModal();
   const loginModal = useLoginModal();
+  const rentModal = useRentModal();
+
   const [isOpen, setIsOpen] = useState(false);
 
   const toggleOpen = useCallback(() => {
     setIsOpen((value) => !value);
   }, []);
 
+  const onRent = useCallback(() => {
+    if (!currentUser) {
+      return loginModal.onOpen();
+    }
+
+    // Open Rent Modal
+    rentModal.onOpen();
+  }, [currentUser, loginModal, rentModal])
+
   return ( 
     <div className="relative">
       <div className="flex flex-row items-center gap-3">
         <div
-          onClick={() => {}}
+          onClick={onRent}
           className="
             hidden
             md:block
@@ -105,7 +117,7 @@ const UserMenu: React.FC<UserMenuProps> = ({
                   label='My Properties'
                 />
                 <MenuItem
-                  onClick={ ()=>{} }
+                  onClick={ rentModal.onOpen }
                   label='Airbnb my home'
                 />
                 <MenuItem
--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import { Nunito } from 'next/font/google'
 import ToasterProvider from './providers/ToasterProvider'
 import LoginModal from './components/elements/Modals/LoginModal/LoginModal'
 import getCurrentUser from './actions/getCurrentUser'
+import RentModal from './components/elements/Modals/RentModal/RentModal'
 
 const font = Nunito({ subsets: ['latin'] })
 
@@ -26,6 +27,7 @@ export default async function RootLayout({
     <html lang="en">
       <body className={font.className}>
         <ToasterProvider />
+        <RentModal />
         <LoginModal />
         <RegisterModal />
         <Navbar currentUser={currentUser}/>
